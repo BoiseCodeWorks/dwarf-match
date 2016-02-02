2c1b10491bde6ae68e9a3ab9d7bf779b4a5caa7d
Brett Steckman <eveningside@gmail.com>
2016-02-02 15:15:24 -0700

    working

--- app/game-controller.js
+++ app/game-controller.js
@@ -1,10 +1,79 @@
 app.controller('GameController', function ($scope, $timeout, GameService) {
+    $scope.deck = GameService.getDeck();
+    $scope.card1 = null;
+    $scope.card2 = null;
+    $scope.totalMatches = 0;
+    $scope.attempts = 0;
+    $scope.victory = false;
+
+    $scope.selectCard = function (card) {
+
+        if ($scope.card1) {
+            $scope.card2 = card;
+            card.show = true
+        } else {
+            $scope.card1 = card;
+            card.show = true;
+            return
+        }
+
+        $timeout(function () {
+
+            if (!isMatch($scope.card1, $scope.card2)) {
+                $scope.card1.show = false;
+                $scope.card2.show = false;
+            }
+            resetCards();
+            $scope.attempts++
+        }, 500)
+
+
+    }
+
+
+
+    function isMatch(card1, card2) {
+        if (card1.title === card2.title) {
+            $scope.totalMatches++;
+            checkVictory();
+            $scope.card1.show = true;
+            $scope.card2.show = true;
+            return true;
+            
+        }
+        
+    }
+
+    function resetCards() {
+
+        $scope.card1 = null;
+        $scope.card2 = null;
+
+    }
+
+    function checkVictory(){
+        if ($scope.totalMatches === 12){
+            $scope.victory = true;
+        }
+    }
+
+
+})
+
+
+
+
+
+
+
+
+
+
+        //Create two card variables to keep track of the current selections
+        //Add to $scope a way to track number of guesses, and total matches
 	
-	//Create two card variables to keep track of the current selections
-	//Add to $scope a way to track number of guesses, and total matches
-	
-	//This is a freebie we are using the GameService to help keep our controller clean. The GameServie will be in charge of creating and shuffling the deck.
-	$scope.deck = GameService.getDeck();
+        //This is a freebie we are using the GameService to help keep our controller clean. The GameServie will be in charge of creating and shuffling the deck.
+        // $scope.deck = GameService.getDeck();
 	
 	
 	//Write a function that accepts a card object on click.
@@ -29,5 +98,3 @@ app.controller('GameController', function ($scope, $timeout, GameService) {
 	
 	
 	//Bonus: Write a function that can reset the game
-	
-});
