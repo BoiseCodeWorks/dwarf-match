0fc7e1427031ba145c9c22e8569a4099d00e25cf
McSkwid <zmixon91@gmail.com>
2016-02-09 22:25:27 -0700

    draft

--- app/game-controller.js
+++ app/game-controller.js
@@ -1,50 +1,85 @@
 app.controller('GameController', function ($scope, $timeout, GameService) {
 	
+    // This is a freebie we are using the GameService to help keep our controller clean. The GameServie will be in charge of creating and shuffling the deck.
+    $scope.deck = GameService.getDeck();
+    
     // Create two card variables on $scope. These will be responsible
     // for keeping track of our selections as we click cards.
     
+    $scope.card1 = null;
+    $scope.card2 = null;
     
+    // Next we need to initate a few more variables on $scope for later use
+    // Let's add variables for tracking the number of guesses (pairs flipped),
+    // for the total number of correct guesses (pairs matched) and finally a
+    // victory boolean to let our controller know if we've won. Refer to the index.html
+    // for variable names
     
-    // Next write a function that accepts a card object on click and
+    $scope.attempts = 0;
+    $scope.totalMatches = 0;
+    $scope.victory = false;
+    
+    // Next write a selectCard function on $scope that accepts a card object on click and
     // lets make it set card.show to true (boolean). Give it a test!
     // After you complete this refer back to readme.md
     
+    $scope.selectCard = function (card) {
+        if (card.show != true) {
+            card.show = true;
+            if (!$scope.card1) {
+                $scope.card1 = card;
+                return;
+            } else if (!$scope.card2) {
+                $scope.card2 = card;
+                if ($scope.isMatch($scope.card1, $scope.card2)) {
+                    $scope.resetCards();
+                } else {
+                    var card1 = $scope.card1;
+                    var card2 = $scope.card2;
+                    $timeout(function () {
+                        card1.show = false;
+                        card2.show = false;
+                    }, 1000);
+                    $scope.resetCards();
+                }
+            }
+        }
+    };
     
+    // Write a resetCards function on $scope that will empty our card variables
+    // and increase the number of attempts
     
-    // Add to $scope a way to track number of guesses, and total matches
+    $scope.resetCards = function () {
+        $scope.card1 = null;
+        $scope.card2 = null;
+        $scope.attempts++;
+    };
 	
-    
-    
-    // This is a freebie we are using the GameService to help keep our controller clean. The GameServie will be in charge of creating and shuffling the deck.
-    
-    $scope.deck = GameService.getDeck();
+    // Next write an isMatch function on $scope that accepts our two cards and if the card titles 
+    // match, increases our totalMatches and returns true else returns false. After this refer 
+    // back to readme.md
 	
-    // Write a function to resetCards
-    // it will empty the two card variables above and increase the number of attempts
-	
+    $scope.isMatch = function (card1, card2) {
+        if (card1.title === card2.title) {
+            $scope.totalMatches++;
+            $scope.checkVictory();
+            return true;
+        } else {
+            return false;
+        }
+    };
     
-	
-    // Write a checkVictory function that will set $scope.victory = true if the totalMatches is half the length of the deck
-	
+    // Finally, write a checkVictory function on $scope that will set $scope.victory = true if the totalMatches 
+    // is half the length of the deck. Tip: the game deck array is available at $scope.deck. When you're done
+    // refer back to readme.md
     
+    $scope.checkVictory = function () {
+        if ($scope.totalMatches >= $scope.deck.length / 2) {
+            $scope.victory = true;
+        }
+    };
     
-    // Write an isMatch function that accepts two cards and returns true or false if the card titles match.
-    
-    
-    
-    // Before assingning card1 or card2 check to make sure both cards are falsey 
-    // This function should set either card1 or card2 depending on the order of selection
-    // set card.show to true
-    // if this is card 1 then return to short circut the function
-    // if card2 and card2 isMatch of card 1 then resetCards() increase the totalMatches and checkVictory()
-    // otherwise this is where we will need to use $timeout with a delay of 1000 
-    // set card1.show = false
-    // card2.show = false
-    // resetCards() 
-	
-    
-    
-    // Bonus: Write a function that can reset the game
+    // Bonus Challenge: Write a function that can reset the game and add a button that calls it
     
     
     
