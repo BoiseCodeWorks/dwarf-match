9461edfdbaf6ac72511e6a08147842b00ee1c392
McSkwid <zmixon91@gmail.com>
2016-02-09 22:40:29 -0700

    fixes scoping and grammar

--- app/game-controller.js
+++ app/game-controller.js
@@ -17,29 +17,29 @@ app.controller('GameController', function ($scope, $timeout, GameService) {
 
     
     // Next write a selectCard function on $scope that accepts a card object on click and
-    // lets make it set card.show to true (boolean). Give it a test!
+    // let's make it set card.show to true (boolean). Give it a test!
     // After you complete this refer back to readme.md
     
 
     
-    // Write a resetCards function on $scope that will empty our card variables
+    // Write a local resetCards function that will empty our card variables
     // and increase the number of attempts
     
 
 	
-    // Next write an isMatch function on $scope that accepts our two cards and if the card titles 
+    // Next write a local isMatch function that accepts our two cards and if the card titles 
     // match, increases our totalMatches and returns true else returns false. After this refer 
     // back to readme.md
 	
     
     
-    // Finally, write a checkVictory function on $scope that will set $scope.victory = true if the totalMatches 
+    // Finally, write a local checkVictory function that will set $scope.victory = true if the totalMatches 
     // is half the length of the deck. Tip: the game deck array is available at $scope.deck. When you're done
     // refer back to readme.md
     
 
     
-    // Bonus Challenge: Write a function that can reset the game and add a button that calls it
+    // Bonus Challenge: Write a function on $scope that can reset the game and add a button that calls it
     
     
     
