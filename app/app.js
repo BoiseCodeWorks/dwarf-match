35e1bd89b86ab3af001ddf9c3011aaa1b22f3032
Jake Overall <joverall22@gmail.com>
2016-07-19 00:18:34 -0600

    upgrade to ng-components

--- app/app.js
+++ app/app.js
@@ -1,1 +1,5 @@
-var app = angular.module('dwarfMatch', []);
+;(function () {
+
+	angular.module('dwarfMatch', [])
+
+}())
--- app/components/game/game.js
+++ app/components/game/game.js
@@ -1,46 +1,41 @@
-app.controller('GameController', function ($scope, $timeout, GameService) {
-	
-    // This is a freebie we are using the GameService to help keep our controller clean. The GameServie will be in charge of creating and shuffling the deck.
-    $scope.deck = GameService.getDeck();
-    
-    // Create two card variables on $scope. These will be responsible
-    // for keeping track of our selections as we click cards.
-    
+;(function () {
+  angular.module('dwarfMatch')
+    .component('gameComponent', {
+      controller: GameController,
+      templateUrl: 'app/components/game/game.html'
+    })
 
-    
-    // Next we need to initate a few more variables on $scope for later use
+  function GameController ($timeout, GameService) {
+    var gc = this
+
+    // This is a freebie we are using the GameService to help keep our controller clean. The GameServie will be in charge of creating and shuffling the deck.
+    gc.deck = GameService.getDeck()
+
+    // Create two card variables on gc. These will be responsible
+    // for keeping track of our selections as we click cards.
+
+    // Next we need to initate a few more variables on gc for later use
     // Let's add variables for tracking the number of guesses (pairs flipped),
     // for the total number of correct guesses (pairs matched) and finally a
     // victory boolean to let our controller know if we've won. Refer to the index.html
     // for variable names
-    
 
-    
-    // Next write a selectCard function on $scope that accepts a card object on click and
+    // Next write a selectCard function on gc that accepts a card object on click and
     // let's make it set card.show to true (boolean). Give it a test!
     // After you complete this refer back to readme.md
-    
 
-    
     // Write a local resetCards function that will empty our card variables
     // and increase the number of attempts
-    
 
-	
     // Next write a local isMatch function that accepts our two cards and if the card titles 
     // match, increases our totalMatches and returns true else returns false. After this refer 
     // back to readme.md
-	
-    
-    
-    // Finally, write a local checkVictory function that will set $scope.victory = true if the totalMatches 
-    // is half the length of the deck. Tip: the game deck array is available at $scope.deck. When you're done
-    // refer back to readme.md
-    
 
-    
-    // Bonus Challenge: Write a function on $scope that can reset the game and add a button that calls it
-    
-    
-    
-});
+    // Finally, write a local checkVictory function that will set gc.victory = true if the totalMatches 
+    // is half the length of the deck. Tip: the game deck array is available at gc.deck. When you're done
+    // refer back to readme.md
+
+    // Bonus Challenge: Write a function on gc that can reset the game and add a button that calls it
+
+  }
+}())
