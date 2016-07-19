f56c5efe7fe7c4f22bf4adc44d684b6f304ee980
Jake Overall <joverall22@gmail.com>
2016-07-19 00:48:08 -0600

    fixes game-service, updates for $ctrl

--- app/components/game/game-service.js
+++ app/components/game/game-service.js
@@ -46,13 +46,12 @@
       this.getDeck = function () {
         var pairs = shuffle(new Deck().cards).concat(shuffle(new Deck().cards))
         var shuffled = shuffle(shuffle(pairs))
-
         return shuffled
       }
 
       function shuffle (deck) {
-        for (var j, x, i = deck.length; i; j = Math.floor(Math.random() * i), x = deck[--i], deck[i] = deck[j], deck[j] = x)
-          return deck
+        for (var j, x, i = deck.length; i; j = Math.floor(Math.random() * i), x = deck[--i], deck[i] = deck[j], deck[j] = x) {}
+        return deck
       }
     })
 }())
